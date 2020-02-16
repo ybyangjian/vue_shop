49ec6bcf4380b8717b389c510b8ab624c8e6a523
ybyangjian <809127232@qq.com>
2020-02-16 22:03:12 +0800

    完成商品功能

--- src/main.js
+++ src/main.js
@@ -16,6 +16,14 @@ import 'element-ui/lib/theme-chalk/index.css'
 // table全局注册
 import TreeTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
+
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 
@@ -32,6 +40,24 @@ Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+
+// 过滤器  这是一个时间过滤器
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  // padStart 指定字符串长度，如果不足两位用0来填充
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
